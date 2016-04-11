448580f667dc4a83a9950c9582730911838eaeeb
Erik Jung <erik@cloudfour.com>
2016-04-11 12:10:43 -0700

    Add tests for additional files

--- test/app.js
+++ test/app.js
@@ -5,17 +5,36 @@
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const GENERATOR_PATH = path.join(__dirname, '../generators/app');
 
 describe('generator-drizzle:app', () => {
+  const answerFixture = {
+    title: 'Test title',
+    description: 'Test description'
+  };
+
   before(done => {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+    helpers.run(GENERATOR_PATH)
+      .withPrompts(answerFixture)
       .on('end', done);
   });
 
-  it('creates files', () => {
+  it('Creates a valid package.json', () => {
+    assert.jsonFileContent('package.json', {
+      description: answerFixture.description
+    });
+  });
+
+  it('Creates a README', () => {
+    assert.fileContent([
+      ['README.md', `# ${answerFixture.title}`]
+    ]);
+  });
+
+  it('Creates a GitHub templates', () => {
     assert.file([
-      'dummyfile.txt'
+      '.github/ISSUE_TEMPLATE.md',
+      '.github/PULL_REQUEST_TEMPLATE.md'
     ]);
   });
 });
