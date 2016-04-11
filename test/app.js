d26e140b9a29129df74606a268b779ac8bf67c87
Erik Jung <erik@cloudfour.com>
2016-04-11 12:15:16 -0700

    Fix a typos

--- test/app.js
+++ test/app.js
@@ -31,7 +31,7 @@ describe('generator-drizzle:app', () => {
     ]);
   });
 
-  it('Creates a GitHub templates', () => {
+  it('Creates GitHub templates', () => {
     assert.file([
       '.github/ISSUE_TEMPLATE.md',
       '.github/PULL_REQUEST_TEMPLATE.md'
